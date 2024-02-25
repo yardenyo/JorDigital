f0b9abd05dbddfb808122c8a83fc9aa6a6b1b1bc
yardenyo <gmloler@gmail.com>
2024-02-25 18:17:20 +0200

    changes

--- src/app/slices/pageSlice.ts
+++ src/app/slices/pageSlice.ts
@@ -6,12 +6,14 @@ export interface PageState {
   currentPage: number;
   previousPage: number;
   nextPage: number;
+  count: number;
 }
 
 const initialState: PageState = {
   currentPage: 1,
   previousPage: 0,
   nextPage: 2,
+  count: 1,
 };
 
 export const pageSlice = createSlice({
@@ -23,15 +25,19 @@ export const pageSlice = createSlice({
       state.currentPage = action.payload;
       state.nextPage = action.payload + 1;
     },
+    setCount: (state, action: PayloadAction<number>) => {
+      state.count = action.payload;
+    },
   },
 });
 
 // Actions
-export const { setPage } = pageSlice.actions;
+export const { setPage, setCount } = pageSlice.actions;
 
 // Selectors
 export const currentPage = (state: RootState) => state.page.currentPage;
 export const previousPage = (state: RootState) => state.page.previousPage;
 export const nextPage = (state: RootState) => state.page.nextPage;
+export const count = (state: RootState) => state.page.count;
 
 export default pageSlice.reducer;
